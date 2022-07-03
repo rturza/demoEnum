109b1e392a379f350522eaf4a31ab35e214e16e2
Turza <cse.turza@gmail.com>
2022-07-03 10:57:55 +0600

    Add project files.

---
+++ demoEnum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoEnum
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+        }
+    }
+}
